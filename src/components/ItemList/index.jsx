81afbcb918a3112b30c89a510d0b4bd4541197d5
Allyson <allysonkaka@gmail.com>
2023-12-20 15:14:53 -0300

    feat: primeiro commit do projeto, está completo, mas sem minhas customizações

---
+++ src/components/ItemList/index.jsx
@@ -0,0 +1,14 @@
+import './styles.css'
+function itemList({title, description}) {
+    
+
+    return (
+        <div className='item-list'>
+            <strong>{title}</strong>
+            <p>{description}</p>
+            <hr></hr>
+        </div>
+    )
+}
+
+export default itemList
